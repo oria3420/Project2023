56543e7fea10b49cf413474f57490a45324ced55
Oriya Yehudai <oria3420@gmail.com>
2023-03-14 14:43:41 +0200

    added a note

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import jwt_decode from "jwt-decode";
 import './Connect.css';
-
+// hey perki
 function LoginPage() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
