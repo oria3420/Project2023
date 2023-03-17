e25e4bffbf39a95d0f8a49dd368cead2615e2b3a
Oriya Yehudai <oria3420@gmail.com>
2023-03-17 13:28:41 +0200

    search recipe page

--- client/src/pages/SearchRecipe.js
+++ client/src/pages/SearchRecipe.js
@@ -2,13 +2,24 @@ import Navbar from '../components/Navbar';
 import { useLocation } from 'react-router-dom';
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import Table from '../tables/Table';
 
 const SearchRecipe = () => {
     const location = useLocation();
     const name = location.state.name;
     const [categories, setCategories] = useState([]);
     const [expandedCategories, setExpandedCategories] = useState({});
-    
+    const [recipes, setRecipes] = useState([]);
+
+
+    useEffect(() => {
+      fetch(`http://localhost:1337/api/table/recipes`)
+        .then(res => res.json())
+        .then(data => setRecipes(data))
+        .catch(error => console.error(error))
+    }, []);
+    console.log(recipes)
+
     useEffect(() => {
         fetch('http://localhost:1337/api/home/search_recipe')
             .then(response => response.json())
@@ -22,7 +33,6 @@ const SearchRecipe = () => {
             })
             .catch(error => console.error(error));
     }, []);
-    console.log(categories)
 
     const toggleCategory = (category) => {
         setExpandedCategories({
@@ -34,21 +44,28 @@ const SearchRecipe = () => {
     return (
         <div>
             {name && <Navbar name={name} />}
-            <div className='filter-menu'>
-                {Object.keys(categories).map((category) => (
-                    <div className='category' key={category}>
-                        <div className="category-header" onClick={() => toggleCategory(category)}>
-                            <span className='category-title'>{category}</span>
-                            <button className="btn btn-light category-toggle-btn">{expandedCategories[category] ? "-" : "+"}</button>
-                        </div>
-                        {expandedCategories[category] && categories[category].map((value) => (
-                            <div className="form-check" key={value}>
-                                <input className="form-check-input" type="checkbox" id={`checkbox_${value}`} />
-                                <label className="form-check-label" htmlFor={`checkbox_${value}`}>{value}</label>
+            <div className='search-recipe-container'>
+                <div className='filter-menu'>
+                    {Object.keys(categories)
+                        .sort((a, b) => a.localeCompare(b))
+                        .map((category) => (
+                            <div className='category' key={category}>
+                                <div className="category-header" onClick={() => toggleCategory(category)}>
+                                    <span className='category-title'>{category}</span>
+                                    <button className="btn btn-light category-toggle-btn">{expandedCategories[category] ? "-" : "+"}</button>
+                                </div>
+                                {expandedCategories[category] && categories[category].map((value) => (
+                                    <div className="form-check" key={value}>
+                                        <input className="form-check-input" type="checkbox" id={`checkbox_${value}`} />
+                                        <label className="form-check-label" htmlFor={`checkbox_${value}`}>{value}</label>
+                                    </div>
+                                ))}
                             </div>
                         ))}
-                    </div>
-                ))}
+                </div>
+                <div className='recipes-container'>
+                    <Table rows={recipes} />
+                </div>
             </div>
         </div>
     )
