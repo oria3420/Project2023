ce4983e0b829d889d3756ae3906a3bea6a2fd7ba
Oriya Yehudai <oria3420@gmail.com>
2023-03-17 12:31:50 +0200

    menu filter

--- client/src/pages/SearchRecipe.js
+++ client/src/pages/SearchRecipe.js
@@ -6,15 +6,30 @@ import React, { useState, useEffect } from 'react';
 const SearchRecipe = () => {
     const location = useLocation();
     const name = location.state.name;
-    const [categories, setCategories] = useState([])
-
+    const [categories, setCategories] = useState([]);
+    const [expandedCategories, setExpandedCategories] = useState({});
+    
     useEffect(() => {
         fetch('http://localhost:1337/api/home/search_recipe')
             .then(response => response.json())
-            .then(data => setCategories(data))
+            .then(data => {
+                const expandedCategories = {};
+                Object.keys(data).forEach(category => {
+                    expandedCategories[category] = false;
+                });
+                setCategories(data);
+                setExpandedCategories(expandedCategories);
+            })
             .catch(error => console.error(error));
     }, []);
     console.log(categories)
+
+    const toggleCategory = (category) => {
+        setExpandedCategories({
+            ...expandedCategories,
+            [category]: !expandedCategories[category]
+        });
+    };
 
     return (
         <div>
@@ -22,12 +37,14 @@ const SearchRecipe = () => {
             <div className='filter-menu'>
                 {Object.keys(categories).map((category) => (
                     <div className='category' key={category}>
-                        <span className='category-title'>{category}</span>
-                        <br />
-                        {categories[category].map((value) => (
-                            <div className="form-check">
-                                <input className="form-check-input" type="checkbox" id="inlineCheckbox1" />
-                                <label className="form-check-label" for="inlineCheckbox1">{value}</label>
+                        <div className="category-header" onClick={() => toggleCategory(category)}>
+                            <span className='category-title'>{category}</span>
+                            <button className="btn btn-light category-toggle-btn">{expandedCategories[category] ? "-" : "+"}</button>
+                        </div>
+                        {expandedCategories[category] && categories[category].map((value) => (
+                            <div className="form-check" key={value}>
+                                <input className="form-check-input" type="checkbox" id={`checkbox_${value}`} />
+                                <label className="form-check-label" htmlFor={`checkbox_${value}`}>{value}</label>
                             </div>
                         ))}
                     </div>
