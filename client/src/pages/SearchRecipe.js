e3e32048dbb458dd90269a80cdfb8715513f2fad
Oriya Yehudai <oria3420@gmail.com>
2023-04-04 16:04:36 +0300

    more fixes

--- client/src/pages/SearchRecipe.js
+++ client/src/pages/SearchRecipe.js
@@ -3,7 +3,7 @@ import RecipeCard from '../components/RecipeCard';
 import { useLocation } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import './App.css';
-import React, { useState, useEffect, useCallback} from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 const SearchRecipe = () => {
     const location = useLocation();
@@ -16,7 +16,7 @@ const SearchRecipe = () => {
     const [filteredRecipes, setFilteredRecipes] = useState([]);
     const [recipesCategories, setRecipesCategories] = useState([]);
 
-      
+
     useEffect(() => {
         fetch(`http://localhost:1337/api/table/recipes`)
             .then(res => res.json())
@@ -50,7 +50,7 @@ const SearchRecipe = () => {
             })
             .catch(error => console.error(error));
     }, []);
-   
+
     const toggleCategory = (category) => {
         setExpandedCategories({
             ...expandedCategories,
@@ -69,47 +69,47 @@ const SearchRecipe = () => {
         let filteredIds = {};
         let anyChecked = false;
         Object.keys(checkedItems).forEach((category) => {
-          Object.keys(checkedItems[category]).forEach((value) => {
-            if (checkedItems[category][value]) {
-              anyChecked = true;
-              const recipeCategory = "recipe_" + category;
-              let category_ID;
-              for (let i = 0; i < categories[category].length; i++) {
-                if (categories[category][i][1] === value.substring(2)) {
-                  category_ID = categories[category][i][0];
+            Object.keys(checkedItems[category]).forEach((value) => {
+                if (checkedItems[category][value]) {
+                    anyChecked = true;
+                    const recipeCategory = "recipe_" + category;
+                    let category_ID;
+                    for (let i = 0; i < categories[category].length; i++) {
+                        if (categories[category][i][1] === value.substring(2)) {
+                            category_ID = categories[category][i][0];
+                        }
+                    }
+                    const tempFilteredIds = {};
+                    for (let i = 0; i < recipesCategories[recipeCategory].length; i++) {
+                        const recipe = recipesCategories[recipeCategory][i];
+                        if (recipe.category_ID === category_ID) {
+                            tempFilteredIds[recipe.recipe_ID] = true;
+                        }
+                    }
+                    if (Object.keys(filteredIds).length === 0) {
+                        filteredIds = tempFilteredIds;
+                    } else {
+                        filteredIds = Object.keys(filteredIds)
+                            .filter((id) => tempFilteredIds[id])
+                            .reduce((obj, id) => {
+                                obj[id] = true;
+                                return obj;
+                            }, {});
+                    }
                 }
-              }
-              const tempFilteredIds = {};
-              for (let i = 0; i < recipesCategories[recipeCategory].length; i++) {
-                const recipe = recipesCategories[recipeCategory][i];
-                if (recipe.category_ID === category_ID) {
-                  tempFilteredIds[recipe.recipe_ID] = true;
-                }
-              }
-              if (Object.keys(filteredIds).length === 0) {
-                filteredIds = tempFilteredIds;
-              } else {
-                filteredIds = Object.keys(filteredIds)
-                  .filter((id) => tempFilteredIds[id])
-                  .reduce((obj, id) => {
-                    obj[id] = true;
-                    return obj;
-                  }, {});
-              }
-            }
-          });
+            });
         });
         if (!anyChecked) {
-          setFilteredRecipes(recipes);
+            setFilteredRecipes(recipes);
         } else {
-          const filteredRecipes = recipes.filter((recipe) => filteredIds[recipe.RecipeId]);
-          setFilteredRecipes(filteredRecipes);
+            const filteredRecipes = recipes.filter((recipe) => filteredIds[recipe.RecipeId]);
+            setFilteredRecipes(filteredRecipes);
         }
-      }, [checkedItems, categories, recipes, recipesCategories]);
-      
-      useEffect(() => {
+    }, [checkedItems, categories, recipes, recipesCategories]);
+
+    useEffect(() => {
         filterRecipes();
-      }, [checkedItems, filterRecipes]);
+    }, [checkedItems, filterRecipes]);
 
     const handleClick = (recipeId) => {
         navigate(`/recipes/${recipeId}`);
@@ -130,7 +130,7 @@ const SearchRecipe = () => {
                                 </div>
                                 {expandedCategories[category] && categories[category].sort((a, b) => a[1].localeCompare(b[1])).map((value) => (
                                     <div className="form-check" key={value}>
-                                        <input className="form-check-input" type="checkbox" id={`checkbox_${value}`} checked={checkedItems[category][value]} onChange={() => handleCheckboxChange(category, value)} />
+                                        <input className="form-check-input" type="checkbox" id={`checkbox_${value}`} defaultChecked={checkedItems[category][value]} onChange={() => handleCheckboxChange(category, value)} />
                                         <label className="form-check-label" htmlFor={`checkbox_${value}`}>{value[1]}</label>
                                     </div>
                                 ))}
