8b1ca2737e0e5e2e62faa8a6fa13d2fe8ef6f1e4
Oriya Yehudai <oria3420@gmail.com>
2023-09-22 12:55:43 +0300

    stars design

--- client/src/components/StarRating.js
+++ client/src/components/StarRating.js
@@ -1,29 +1,30 @@
 import React from 'react';
+import './StarRating.css';
 
 function StarRating({ rating }) {
-  const maxRating = 5; 
-  const filledStars = Math.floor(rating);
-  const unfilledStars = maxRating - filledStars;
+    const maxRating = 5;
+    const filledStars = Math.floor(rating);
+    const unfilledStars = maxRating - filledStars;
 
-  const stars = [];
+    const stars = [];
 
-  for (let i = 0; i < filledStars; i++) {
-    stars.push(
-      <span key={i} className="star filled-star">
-        ★
-      </span>
-    );
-  }
+    for (let i = 0; i < filledStars; i++) {
+        stars.push(
+            <span key={i} className='star  filled-star'>
+            ★
+            </span>
+        );
+    }
 
-  for (let i = 0; i < unfilledStars; i++) {
-    stars.push(
-      <span key={i + filledStars} className="star unfilled-star">
-        ☆
-      </span>
-    );
-  }
+    for (let i = 0; i < unfilledStars; i++) {
+        stars.push(
+            <span key={i + filledStars} className='star unfilled-star'>
+            ★
+            </span>
+        );
+    }
 
-  return <div className="rating-stars">{stars}</div>;
+    return <div className="rating-stars">{stars}</div>;
 }
 
 export default StarRating;
