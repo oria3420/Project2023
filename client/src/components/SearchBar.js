193b040e615e284366bb4ddb2b04f910628d62be
Oriya Yehudai <oria3420@gmail.com>
2023-11-30 13:20:45 +0200

    search box design

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,6 +1,7 @@
-// SearchBar.js
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './Components.css'
 
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -9,33 +10,33 @@ const SearchBar = () => {
   const handleSearch = (e) => {
     e.preventDefault();
     if (!searchTerm.trim()) {
-      // Handle empty search term (e.g., show a message, prevent navigation)
       console.log('Search term is empty. Please enter a search term.');
       return;
     }
-    // Navigate to a new page with the search results
     console.log(searchTerm)
     navigate(`/search_recipe?query=${searchTerm}`);
-     // Clear the search term in the input field
     setSearchTerm('');
   };
 
   return (
     <form className="form-inline" onSubmit={handleSearch}>
+    <div className="input-group">
       <input
-        className="form-control mr-sm-2"
+        className="search-input form-control mr-sm-2"
         type="search"
         placeholder="Search"
         aria-label="Search"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <button className="btn btn-primary" type="submit">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
-      <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-    </svg>
-      </button>
-    </form>
+      <div className="input-side input-group-append">
+        <button className="btn" type="submit">
+          <i className="search-icon bi bi-search"></i>
+        </button>
+      </div>
+    </div>
+  </form>
+  
   );
 };
 
