97f0ab6f69788534a9dbcc26cbf8beeccfbe4e19
Oriya Yehudai <oria3420@gmail.com>
2023-12-03 13:42:51 +0200

    welcome page design

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -23,16 +23,19 @@ function WelcomePage() {
       </div>
 
       <div className="welcome-connect">
-        <img src="/images/logo_black_english.png" alt="Logo" className="logo-welcome" />
-        <h1 className="welcome-title">WELCOME TO
-        <br></br>
-        MA
-        MITBASHEL</h1>
-        <h3 className='sub-title'>Get started</h3>
-        <input id="btn-login-welcome" className="btn btn-primary" type="submit" value="Login" onClick={redirectToLogin} />
-        <input id="btn-register-welcome" className="btn btn-primary" type="button" value="Register" onClick={redirectToRegister} />
+        <img src="/images/logo_white_english.png" alt="Logo" className="logo-welcome" />
+        <div className='welcome-connect-body'>
+          <h1 className="welcome-title-up">Cook, Create, Enjoy:</h1>
+          <h1 className="welcome-title-bottom">Welcome to
+            <br></br>
+            Ma Mitbashel</h1>
+          <h3 className='sub-title'>Get started</h3>
+          <div className='buttons-container'>
+            <input id="btn-login-welcome" className="btn btn-primary" type="submit" value="Login" onClick={redirectToLogin} />
+            <input id="btn-register-welcome" className="btn btn-primary" type="button" value="Register" onClick={redirectToRegister} />
+          </div>
+        </div>
       </div>
-
     </div>
   );
 }
