75ca4693d3b61a0199562b00949d51fd48a3e599
Oriya Yehudai <oria3420@gmail.com>
2023-12-04 13:26:04 +0200

    drop down menu design

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -48,7 +48,7 @@ const Navbar = ({ name }) => {
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
                     </svg>
                 </div>
-                <div className='name-container'>
+                <div className='name-container' role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
                         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                         <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
@@ -56,7 +56,7 @@ const Navbar = ({ name }) => {
 
                     <div className="dropdown">
 
-                        <span className="navbar-text dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <span className="navbar-text dropdown-toggle" >
                             {name}
                         </span>
 
