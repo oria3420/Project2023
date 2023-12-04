1aa5e4011287435c4554db05c0245b4dde5c2738
Oriya Yehudai <oria3420@gmail.com>
2023-12-04 13:04:56 +0200

    nurition table

--- client/src/components/NutritionTable.js
+++ client/src/components/NutritionTable.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Components.css';
 
 const NutritionTable = ({ recipe }) => {
@@ -14,6 +14,20 @@ const NutritionTable = ({ recipe }) => {
         { key: 'ProteinContent', label: 'Protein' },
     ];
 
+    useEffect(() => {
+        // Find the maximum width among all .nutrition-field elements
+        const nutritionFieldsElements = document.querySelectorAll('.nutrition-field');
+        let maxWidth = 0;
+
+        nutritionFieldsElements.forEach((field) => {
+            maxWidth = Math.max(maxWidth, field.clientWidth);
+        });
+
+        // Set the width for all .nutrition-field elements
+        nutritionFieldsElements.forEach((field) => {
+            field.style.width = maxWidth + 'px';
+        });
+    }, []); // The empty dependency array ensures that this effect runs only once after the initial render
 
     return (
         <div className='nutrition-container'>
@@ -24,24 +38,15 @@ const NutritionTable = ({ recipe }) => {
 
             <div className='nutrition-table'>
                 {nutritionFields.map((field, index) => (
-
                     <React.Fragment key={field.key}>
-
                         <div className='nutrition-field'>
                             <span className='field-value'>{recipe[field.key]}</span>
-
                             <br />
-
                             <span className='field-label'>{field.label}</span>
-
-
                         </div>
                         {index < nutritionFields.length - 1 && <div className='nutrition-divider'></div>}
-
                     </React.Fragment>
-
                 ))}
-
             </div>
         </div>
     );
