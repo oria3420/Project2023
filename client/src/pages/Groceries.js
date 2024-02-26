b61186f90c21cd37a30f5a6b5c9c5e13988ed969
Shirataitel <perki.tayytel100@gmail.com>
2024-02-26 20:05:05 +0200

    a

--- client/src/pages/Groceries.js
+++ client/src/pages/Groceries.js
@@ -3,6 +3,7 @@ import './App.css';
 import React, { useState, useEffect} from 'react';
 import jwt_decode from "jwt-decode";
 import { useNavigate } from 'react-router-dom'
+import './Connect.css';
 
 const Groceries = () => {
     const navigate = useNavigate()
